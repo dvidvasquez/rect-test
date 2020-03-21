f432fc6e67f3f4b165efab4cd80ac44a6b82ddcb
David Vasquez <davidvasquez.s@hotmail.com>
2020-03-20 20:38:06 -0500

    Updates

---
+++ precache-manifest.237bb24595dc0695f3e672f908475ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba70b5ebb6a019bc7b81e9298e3a58bb",
+    "url": "//dvidvasquez.github.io/react-test/index.html"
+  },
+  {
+    "revision": "9092fc38c1db3d6f61f7",
+    "url": "//dvidvasquez.github.io/react-test/static/css/main.1c5cab03.chunk.css"
+  },
+  {
+    "revision": "b15ecc5e7a36db61a1ac",
+    "url": "//dvidvasquez.github.io/react-test/static/js/2.7c242059.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "//dvidvasquez.github.io/react-test/static/js/2.7c242059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9092fc38c1db3d6f61f7",
+    "url": "//dvidvasquez.github.io/react-test/static/js/main.0f7a3c13.chunk.js"
+  },
+  {
+    "revision": "7c6ed1ed2a0b966d33df",
+    "url": "//dvidvasquez.github.io/react-test/static/js/runtime-main.76a6a477.js"
+  }
+]);
