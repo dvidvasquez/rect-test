38a2d35b173e4f825ffe179b22fa005d5a9a4775
David Vasquez <davidvasquez.s@hotmail.com>
2020-03-20 21:25:24 -0500

    Subiendo archivos que faltaron

---
+++ build/precache-manifest.dc2e4b51a7db9b90b8a46a10599ebef4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c0e092a495f79284962ac86be74381",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "dd682cdcae2248abe243",
+    "url": "/react-test/static/css/main.1c5cab03.chunk.css"
+  },
+  {
+    "revision": "b15ecc5e7a36db61a1ac",
+    "url": "/react-test/static/js/2.7c242059.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-test/static/js/2.7c242059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd682cdcae2248abe243",
+    "url": "/react-test/static/js/main.be82cedc.chunk.js"
+  },
+  {
+    "revision": "b3f52e2da6f3a69ddf74",
+    "url": "/react-test/static/js/runtime-main.eaba7fbe.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//dvidvasquez.github.io/react-test/precache-manifest.237bb24595dc0695f3e672f908475ef9.js"
+  "/react-test/precache-manifest.dc2e4b51a7db9b90b8a46a10599ebef4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//dvidvasquez.github.io/react-test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
